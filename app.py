05aff6e0d7f1a1f9f16c7f6af630db981260073b
Devenvaruv <devenvaruv@gmail.com>
2025-07-18 00:16:10 +0530

    first commitd

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -13,4 +14,5 @@ def home():
     return 'Backend is running', 200
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
